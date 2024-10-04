7409ef2a4b5b872795e0d72e475d33218e098b6c
JoaoVitor75 <joaovitorferreira9843@gmail.com>
2024-10-03 21:11:37 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Button>
+          Aperta-me
+        </Button>
+      </div>
+      <p className="read-the-docs">
+      </p>
+    </>
+  )
+}
+
+export default App
