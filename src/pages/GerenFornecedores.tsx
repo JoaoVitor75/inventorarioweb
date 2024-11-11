f1b7372af1886754ef12017b5ea4f74be3d3da5d
PedroAngeloo <paslazzarin@minha.fag.edu.br>
2024-11-11 19:21:32 -0300

    chamando api para vincular com sqlite

--- src/pages/GerenFornecedores.tsx
+++ src/pages/GerenFornecedores.tsx
@@ -50,6 +50,7 @@ export function SuppliersPage() {
       } catch (err) {
         if (err instanceof Error) {
           setError(err.message);
+          
         } else {
           setError("An unknown error occurred");
         }
